0c5c81171a64071068d36766694ffd1f3d5ab046
Dylan Brooks <dpbrooks1414@gmail.com>
2025-02-10 10:23:15 -0500

    Attribute functionality

---
+++ src/AttributeInterface.java
@@ -0,0 +1,14 @@
+
+
+public interface AttributeInterface {
+	
+	/*
+	 * Returns attribute name
+	 */
+	public String getName();
+	
+	/*
+	 * Renames the attribute to the newName
+	 */
+	public void renameAttribute(String newName);
+}
