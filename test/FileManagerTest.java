9a2c37260970be550b5bf6720f90db3c63edbda3
k <markers711@gmail.com>
2025-02-13 12:56:44 -0500

    removed tests for old interface

--- test/FileManagerTest.java
+++ test/FileManagerTest.java
@@ -4,39 +4,21 @@ import java.util.ArrayList;
 // TODO write tests
 class FileManagerSaveTest {
 	FileManager file = new FileManager();
-	ArrayList<RelationshipInterface> r = new ArrayList<RelationshipInterface>();
-	ArrayList<ClassObjectInterface> c = new ArrayList<ClassObjectInterface>();
 
 	@Test
 	public void saveOK() {
-		try {
-			file.save("test.json", r, c);
-		} catch (Exception e) {
-		}
 	}
 
 	@Test
 	public void saveBadPath() {
-		try {
-			file.save("", r, c);
-		} catch (Exception e) {
-		}
 	}
 
 	@Test
 	public void saveBadRelationship() {
-		try {
-			file.save("test.json", null, c);
-		} catch (Exception e) {
-		}
 	}
 
 	@Test
 	public void saveBadClass() {
-		try {
-			file.save("test.json", r, null);
-		} catch (Exception e) {
-		}
 	}
 }
 
