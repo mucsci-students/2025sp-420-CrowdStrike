aca7dc080fa71f6a58713087c3487f64646b17d1
k <markers711@gmail.com>
2025-02-16 12:54:38 -0500

    Working Load and Save with tests

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,19 +1,26 @@
-import java.io.File;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.IOException;
 
 public class FileManager implements FileManagerInterface {
-	public void save(String path, UMLModelInterface model) throws IOException {
-		File jsonFile = new File(path);
-		if (!jsonFile.canWrite())
-			throw new IOException("Can not write to given path.");
-		// TODO
+
+	public void save(String path, UMLModel model) throws Exception {
+		String json;
+		Gson gson = new Gson();
+		FileWriter writer = new FileWriter(path);
+
+		json = gson.toJson(model);
+		writer.write(json);
+		writer.close();
+
 	}
 
-	public UMLModelInterface load(String path) throws IOException {
-		File jsonFile = new File(path);
-		if (!jsonFile.canRead())
-			throw new IOException("Can not read given path.");
-		// TODO
-		return new UMLModel();
+	public UMLModel load(String path) throws Exception {
+		Gson gson = new Gson();
+		String content = Files.readString(Paths.get(path));
+		UMLModel model = gson.fromJson(content, UMLModel.class);
+		return model;
 	}
 }
