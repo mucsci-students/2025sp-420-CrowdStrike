e0563b4b1549102ee436246a0b2ef90d1a938dc1
donut0711 <markers711@gmail.com>
2025-02-27 14:07:44 -0500

    start building test for Load

--- app/src/test/java/org/FileManagerTest.java
+++ app/src/test/java/org/FileManagerTest.java
@@ -3,9 +3,11 @@ package org;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.io.File;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -14,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.Model.UMLModel;
 import org.Model.ClassObject;
 import org.Model.Relationship;
+import org.Controller.UMLEditor;
 
 class FileManagerSaveTest {
 	FileManager file;
@@ -86,23 +89,59 @@ class FileManagerSaveTest {
 
 class FileManagerLoadTest {
 	FileManager file;
+	UMLEditor edit;
 	UMLModel mdl;
 	static String PATH = "./Load.json";
+	static String CONTENT = """
+			{
+			  "classes": [
+			    {
+			      "name": "a",
+			      "fields": [],
+			      "methods": []
+			    },
+			    {
+			      "name": "b",
+			      "fields": [],
+			      "methods": []
+			    },
+			    {
+			      "name": "c",
+			      "fields": [],
+			      "methods": []
+			    }
+			  ],
+			  "relationships": [
+			    {
+			      "source": "a",
+			      "destination": "b",
+			      "type": ""
+			    }
+			  ]
+			}
+			""";
 
 	@BeforeEach
 	void init() {
-		file = new FileManager();
+		// TODO create more full state.
 		mdl = new UMLModel();
-		ClassObject objs[] = { new ClassObject("a"), new ClassObject("b"), new ClassObject("c") };
-		mdl.getClassList().add(objs[0]);
-		mdl.getClassList().add(objs[1]);
-		mdl.getClassList().add(objs[2]);
-		mdl.getRelationshipList().add(new Relationship(objs[0], objs[1]));
-		try {
-			file.save(PATH, mdl);
+		edit = new UMLEditor(mdl);
+		edit.addClass("a");
+		edit.addClass("b");
+		edit.addClass("c");
+		edit.addRelationship("", "a", "b");
+
+		try (FileWriter writer = new FileWriter(PATH)) {
+			writer.write(CONTENT);
 		} catch (Exception e) {
 		}
-		;
+
+	}
+
+	@AfterEach
+	void cleanUp() {
+		File file = new File(PATH);
+		file.delete();
 	}
 
 	@Test
@@ -128,8 +167,14 @@ class FileManagerLoadTest {
 
 	@Test
 	public void loadBadFile() {
+		File file = new File(PATH);
+		file.delete();
+		try (FileWriter writer = new FileWriter(PATH)) {
+			writer.write("{ \"this is bad json\": 69420}");
+		} catch (Exception e) {
+		}
 		try {
-			UMLModel a = file.load("");
+			UMLModel a = this.file.load(PATH);
 			fail("loaded bad file");
 		} catch (Exception e) {
 		}
