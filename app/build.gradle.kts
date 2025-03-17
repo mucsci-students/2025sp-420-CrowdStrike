8356b9161e0cc4dd7a6a48d38a220e51662789a7
donut0711 <markers711@gmail.com>
2025-03-17 15:30:16 -0400

    added JaCoCo to build system for test coverage reporting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+    jacoco
 }
 
 repositories {
@@ -40,6 +41,10 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
 tasks.named<JavaExec>("run") {
