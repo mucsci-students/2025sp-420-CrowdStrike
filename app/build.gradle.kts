040fc34eeb93e1d3b73a7227f19c8f9c7536bde1
donut0711 <markers711@gmail.com>
2025-03-28 16:02:05 -0400

    added picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation("com.google.code.gson:gson:2.12.1")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
