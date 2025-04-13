db8e1883d8e9a1cb64044a22f1eb3cbe5af0833b
donut0711 <markers711@gmail.com>
2025-04-12 20:39:15 -0400

    Added to string to field and method

--- app/src/main/java/org/Model/Method.java
+++ app/src/main/java/org/Model/Method.java
@@ -185,5 +185,23 @@ public class Method implements AttributeInterface {
         throw new IllegalArgumentException("Parameter not found: " + oldParamName);
     }
 
+	@Override
+	public String toString(){
+		StringBuilder r = new StringBuilder();
+		r.append(name);
+
+		r.append("(");
+		for(Parameter p : paramList){
+			r.append(p.getName());
+			r.append(":");
+			r.append(p.getType());
+			r.append(",");
+		}
+		r.deleteCharAt(r.lastIndexOf(","));
+		r.append(")");
+
+		return r.toString();
+	}
+
 
 }
