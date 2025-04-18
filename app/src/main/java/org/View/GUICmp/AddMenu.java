3a0d64d701ef86eed56d9ccc1b0c94d04ea5d1fb
donut0711 <markers711@gmail.com>
2025-04-18 09:27:03 -0400

    Added dummy code to add to spawn in random named class
    Added dummy code to spawn random named class so that the ovserver
    can be made before add class is built.

--- app/src/main/java/org/View/GUICmp/AddMenu.java
+++ app/src/main/java/org/View/GUICmp/AddMenu.java
@@ -1,11 +1,44 @@
 package org.View.GUICmp;
 
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import org.Controller.UMLEditor;
 
 public class AddMenu extends JMenu {
-	public AddMenu(UMLEditor e) {
+	public AddMenu(UMLEditor edit) {
 		super("Add");
+		JMenuItem addClass,addRelationship;
+		addClass = new JMenuItem("Add Class");
+		addRelationship = new JMenuItem("Add Relationship");
+
+		addClass.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent a) {
+			    try {
+				String name;
+				//TODO replace with real ui
+				Random r = new Random();
+				name = "tmp"+new Integer(r.nextInt()).toString();
+				//
+				edit.getModel().isValidClassName(name);
+				edit.addClass(name);
+			    } catch (Exception e){
+			    }
+			}
+		});
+
+		addClass.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    //TODO
+			}
+		});
+
+		add(addClass);
+		add(addRelationship);
 	}
 }
